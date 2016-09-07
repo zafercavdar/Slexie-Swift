ee2b43cb91e9ab6855462c254ea45e4d6796e203
zafercavdar <zafercavdar@yahoo.com>
2016-09-07 20:39:41 +0300

    offline persistance enabled

--- Newsify/AppDelegate.swift
+++ Newsify/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
         return true
     }
 
